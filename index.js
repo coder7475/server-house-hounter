680b37eb39a23a73aacf3d025b7d8ec3eed07f6c
coder7475 <robiulhossain7475@gmail.com>
2024-01-23 07:06:58 +0600

    feat: add necessary middleware

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // modules
+require('dotenv').config();
 const express = require("express");
 const mongoose = require('mongoose');
-require('dotenv').config();
+const cors = require('cors');
 
 // Declaration
 const app = express();
 const port = 3000;
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.xjslrno.mongodb.net/phone-hunter?retryWrites=true&w=majority`;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
 
 // connection to mongodb
 main().catch(err => console.log(err));
@@ -14,7 +19,10 @@ async function main() {
     // await mongoose.connect('mongodb://127.0.0.1:27017/test');
   
     await mongoose.connect(uri);
-    console.log("conected")
+    console.log("Conected to MongoDB!")
+
+    // registration api
+    
 }
 
 app.get("/", (req, res) => {
@@ -22,6 +30,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Phone Hunter app listening on port ${port}`);
 });
 
