223a091a3751baecad6e9d71b0f0078da08459d6
coder7475 <robiulhossain7475@gmail.com>
2024-01-24 10:04:57 +0600

    feat: comment out all

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // modules
 // require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const cors = require("cors");
 // const bcrypt = require("bcrypt");
 // const jwt = require('jsonwebtoken');
