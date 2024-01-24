b442224fb0c485f945e7b4ebc6be890fa506b2d5
coder7475 <robiulhossain7475@gmail.com>
2024-01-24 09:27:06 +0600

    fix: try to fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ main().catch((err) => console.log(err));
 async function main() {
   // await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-  await mongoose.connect(uri);
+  mongoose.connect(uri);
   console.log("Conected to MongoDB!");
 
   // registration api
